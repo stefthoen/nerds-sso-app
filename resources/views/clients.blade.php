27a39c855a1ba2021f64f30314f23fdc0f932fa9
Stef Thoen <stef@baardbaard.nl>
2024-10-28 11:46:35 +0100

    Setup basic flow for creating OATH2 clients

---
+++ resources/views/clients.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Clients') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white p-6 text-gray-900">
+                    {{ __("Here are a list of your clients:") }}
+                    @foreach($clients as $client)
+                        <div class="py-3 text-gray-900">
+                            <h3 class="text-lg text-gray-500">{{ $client->name }}</h3>
+                            <p>{{ $client->redirect }}</p>
+                            <p>{{ $client->secret }}</p>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="/oauth/clients" method="POST">
+                        <div class="mt-2 flex flex-col">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" placeholder="Client name"></input>
+                        </div>
+                        <div class="mt-2 flex flex-col">
+                            <label class="mt-2" for="redirect">Redirect</label>
+                            <input type="text" name="redirect" placeholder="https://my-url.com/callback"></input>
+                        </div>
+                        <div class="mt-4">
+                            @csrf
+                            <button class="bg-gray-600 text-white rounded-full px-4 py-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50" type="submit">Create Client</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
+
+Route::get('/dashboard/clients', function(Request $request) {
+    return view('clients', [
+        'clients' => $request->user()->clients
+    ]);
+})->middleware(['auth', 'verified'])
+    ->name('clients');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
